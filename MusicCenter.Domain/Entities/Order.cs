aca00a5a9330f89704a99609bbb754dfe0a6e55e
Andrej <zarke998@gmail.com>
2021-05-27 18:02:33 +0200

    feat: add Order and OrderProduct entities

---
+++ MusicCenter.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Domain.Entities
+{
+    public class Order : Entity
+    {
+        public string ShippingAdress { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<OrderProduct> OrderProducts { get; set; } = new HashSet<OrderProduct>();
+    }
+}
---
+++ MusicCenter.Domain/Entities/OrderProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Domain.Entities
+{
+    public class OrderProduct : Entity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
