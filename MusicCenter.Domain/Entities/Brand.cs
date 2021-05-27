44380a1719ca52e48d6ec27a56bf26bc033eeb72
Andrej <zarke998@gmail.com>
2021-05-27 16:55:32 +0200

    feat: add Brand entity

---
+++ MusicCenter.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Domain.Entities
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
