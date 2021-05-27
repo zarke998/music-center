2e7cdca16a74975a378647f0d37d2291ccc721db
Andrej <zarke998@gmail.com>
2021-05-27 16:36:38 +0200

    feat: add base Entity

---
+++ MusicCenter.Domain/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
