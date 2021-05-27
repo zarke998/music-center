fa556d312f28f77bb8cf70d972baaf28bb36f836
Andrej <zarke998@gmail.com>
2021-05-27 18:15:53 +0200

    feat: add IQuery interface

---
+++ MusicCenter.Application/Queries/IQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Queries
+{
+    public interface IQuery<TResult> : IUseCase
+    {
+        TResult Execute();
+    }
+
+    public interface IQuery<TParam, TResult> : IUseCase
+    {
+        TResult Execute(TParam param);
+    }
+}
