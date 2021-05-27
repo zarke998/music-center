dfea805feed7fa2e6da4246d3bad7dc43416721e
Andrej <zarke998@gmail.com>
2021-05-27 18:15:34 +0200

    feat: add ICommand interface

---
+++ MusicCenter.Application/Commands/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Commands
+{
+    public interface ICommand : IUseCase
+    {
+        void Execute();
+    }
+
+    public interface ICommand<TParam> : IUseCase
+    {
+        void Execute(TParam param);
+    }
+}
---
+++ MusicCenter.Application/IUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application
+{
+    public interface IUseCase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
