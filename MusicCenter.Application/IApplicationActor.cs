c7acf51dd64bd3e81ab5b6f411ab56a9f1eb3f2a
Andrej <zarke998@gmail.com>
2021-05-30 15:35:08 +0200

    feat: add IApplicationActor interface

---
+++ MusicCenter.Application/IApplicationActor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application
+{
+    public interface IApplicationActor
+    {
+        public int Id { get; set; }
+        public string Identity { get; set; }
+        public ICollection<int> AllowedUseCases { get; set; }
+    }
+}
