12c6a2a46eda9c1dc796ae78dc4f6a3c01a77b17
Andrej <zarke998@gmail.com>
2021-05-30 15:37:18 +0200

    refactor: move ICommand and IQuery to root

--- MusicCenter.Application/ICommand.cs
+++ MusicCenter.Application/ICommand.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicCenter.Application.Commands
+namespace MusicCenter.Application
 {
     public interface ICommand : IUseCase
     {
