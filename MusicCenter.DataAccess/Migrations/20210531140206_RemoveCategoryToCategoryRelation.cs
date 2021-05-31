7076c5135efc4ab190c92e54d7b5e4ee2c141f27
Andrej <zarke998@gmail.com>
2021-05-31 16:04:11 +0200

    feat: remove cat to cat relation

---
+++ MusicCenter.DataAccess/Migrations/20210531140206_RemoveCategoryToCategoryRelation.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicCenter.EfDataAccess.Migrations
+{
+    public partial class RemoveCategoryToCategoryRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ParentId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentId",
+                table: "Categories",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories",
+                column: "ParentId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- MusicCenter.Domain/Entities/Category.cs
+++ MusicCenter.Domain/Entities/Category.cs
@@ -9,10 +9,5 @@ namespace MusicCenter.Domain.Entities
     public class Category : Entity
     {
         public string Name { get; set; }
-
-        public int? ParentId { get; set; }
-        public Category Parent { get; set; }
-
-        public ICollection<Category> Children { get; set; } = new HashSet<Category>();
     }
 }
