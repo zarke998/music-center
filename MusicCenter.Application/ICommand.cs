355d35552fdb187c399df29d03d63c574fae874d
Andrej <zarke998@gmail.com>
2021-06-02 14:55:28 +0200

    feat: remove non generic ICommand

--- MusicCenter.Application/ICommand.cs
+++ MusicCenter.Application/ICommand.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace MusicCenter.Application
 {
-    public interface ICommand : IUseCase
-    {
-        void Execute();
-    }
-
     public interface ICommand<TRequest> : IUseCase
     {
         void Execute(TRequest request);
