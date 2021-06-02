abdf25a8c1870f22361061b510cf02944a287a80
Andrej <zarke998@gmail.com>
2021-06-02 14:44:29 +0200

    feat: add UseCaseExecutor

---
+++ MusicCenter.Application/Exceptions/UnauthorizedUseCaseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MusicCenter.Application.Exceptions
+{
+    [Serializable]
+    public class UnauthorizedUseCaseException : Exception
+    {
+        public UnauthorizedUseCaseException(IUseCase useCase, IApplicationActor actor) : base($"Actor '{actor.Identity}' with id {actor.Id} is not allowed to execute use case '{useCase.Name}' with id {useCase.Id}")
+        {
+
+        }
+    }
+}
---
+++ MusicCenter.Application/UseCaseExecutor.cs
@@ -0,0 +1,46 @@
+﻿using MusicCenter.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;   
+
+namespace MusicCenter.Application
+{
+    public class UseCaseExecutor
+    {
+        private readonly IUseCaseLogger _logger;
+        private readonly IApplicationActor _actor;
+
+        public UseCaseExecutor(IUseCaseLogger logger, IApplicationActor actor)
+        {
+            _logger = logger;
+            this._actor = actor;
+        }
+
+        public void ExecuteCommand<TRequest>(ICommand<TRequest> command, TRequest request)
+        {
+            _logger.Log(command, _actor, request);
+
+            if (_actor.AllowedUseCases.Contains(command.Id))
+            {
+                throw new UnauthorizedUseCaseException(command, _actor);
+            }
+
+            command.Execute(request);
+
+        }
+        public TResult ExecuteQuery<TSearch, TResult>(IQuery<TSearch, TResult> query, TSearch search)
+        {
+            _logger.Log(query, _actor, search);
+
+            if (!_actor.AllowedUseCases.Contains(query.Id))
+            {
+                throw new UnauthorizedUseCaseException(query, _actor);
+            }
+
+            return query.Execute(search);
+        }
+
+    }
+}
