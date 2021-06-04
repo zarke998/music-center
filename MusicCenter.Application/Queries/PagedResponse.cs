822edb4e974df8c280e3668a02f18389b2f004d1
Andrej <zarke998@gmail.com>
2021-06-04 15:09:37 +0200

    feat: add PagedResponse

---
+++ MusicCenter.Application/Queries/PagedResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Queries
+{
+    public class PagedResponse<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int TotalCount { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int PagesCount => (int)Math.Ceiling((double)TotalCount / ItemsPerPage);
+
+        public IEnumerable<T> Items { get; set; }
+    }
+}
