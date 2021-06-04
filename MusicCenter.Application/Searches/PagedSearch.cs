09a09acde6fd8ea30a9b44c0010c855b0160dfc4
Andrej <zarke998@gmail.com>
2021-06-04 14:39:11 +0200

    feat: add ProductSearch

---
+++ MusicCenter.Application/Searches/PagedSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Searches
+{
+    public abstract class PagedSearch
+    {
+        public int? Page { get; set; } = 1;
+        public int? PerPage { get; set; } = 8;
+    }
+}
---
+++ MusicCenter.Application/Searches/ProductSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Searches
+{
+    public class ProductSearch : PagedSearch
+    {
+        public string Name { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string BrandName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
