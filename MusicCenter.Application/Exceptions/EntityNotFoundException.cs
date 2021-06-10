d24ecc6d29cb4e7d1e25c2e8bd35fec82c343ea0
Andrej <zarke998@gmail.com>
2021-06-10 15:16:25 +0200

    feat: add EfGetSingleProductQuery

---
+++ MusicCenter.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(Type entityType, int id) : base($"Entity {entityType.Name} of id {id} not found.")
+        {
+
+        }
+    }
+}
---
+++ MusicCenter.Implementation/Queries/EfGetSingleProductQuery.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MusicCenter.Application.DTO;
+using MusicCenter.Application.Exceptions;
+using MusicCenter.Application.Queries;
+using MusicCenter.Domain.Entities;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Queries
+{
+    public class EfGetSingleProductQuery : IGetSingleProductQuery
+    {
+        private readonly MusicCenterDbContext _context;
+        private readonly IMapper _mapper;
+
+        public int Id => 2;
+        public string Name => "Ef Get Single Product";
+
+        public EfGetSingleProductQuery(MusicCenterDbContext context, IMapper mapper)
+        {
+            this._context = context;
+            this._mapper = mapper;
+        }
+
+        public ProductDto Execute(int id)
+        {
+            var product = _context.Products.Include(p => p.Brand)
+                                            .Include(p => p.ProductCategories).ThenInclude(pc => pc.Category)
+                                            .FirstOrDefault(p => p.Id == id);
+
+            if(product == null)
+            {
+                throw new EntityNotFoundException(typeof(Product), id);
+            }
+
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
