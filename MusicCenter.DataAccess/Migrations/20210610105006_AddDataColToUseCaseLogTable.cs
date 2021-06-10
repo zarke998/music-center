4adcb317aef25c0e7b69455e6f1c4cb88ecaccaa
Andrej <zarke998@gmail.com>
2021-06-10 12:54:34 +0200

    feat(DA): add data col to UseCaseLogs table

---
+++ MusicCenter.DataAccess/Migrations/20210610105006_AddDataColToUseCaseLogTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicCenter.EfDataAccess.Migrations
+{
+    public partial class AddDataColToUseCaseLogTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "UseCaseLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "UseCaseLogs");
+        }
+    }
+}
