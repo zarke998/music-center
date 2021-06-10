421299dc345c30852aba93a2cec59f4a4adc5c33
Andrej <zarke998@gmail.com>
2021-06-10 13:16:41 +0200

    feat: add DatabaseUseCaseLogger

---
+++ MusicCenter.Implementation/Loggers/DatabaseUseCaseLogger.cs
@@ -0,0 +1,37 @@
+﻿using MusicCenter.Application;
+using MusicCenter.Domain.Entities;
+using MusicCenter.EfDataAccess;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Loggers
+{
+    public class DatabaseUseCaseLogger : IUseCaseLogger
+    {
+        private readonly MusicCenterDbContext _context;
+
+        public DatabaseUseCaseLogger(MusicCenterDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Log(IUseCase useCase, IApplicationActor actor, object useCaseData)
+        {
+            var useCaseLog = new UseCaseLog()
+            {
+                UseCaseId = useCase.Id,
+                UseCaseName = useCase.Name,
+                Data = JsonConvert.SerializeObject(useCaseData),
+                CreatedAt = DateTime.UtcNow,
+                UserId = actor.Id
+            };
+
+            _context.UseCaseLogs.Add(useCaseLog);
+            _context.SaveChanges();
+        }
+    }
+}
