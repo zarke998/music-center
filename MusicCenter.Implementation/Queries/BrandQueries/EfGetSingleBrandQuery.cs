12e93605af1a52f63470d98ba7ccaab6686d24c2
Andrej <zarke998@gmail.com>
2021-06-13 22:44:17 +0200

    feat: add EfGetSingleBrandQuery

---
+++ MusicCenter.Implementation/Queries/BrandQueries/EfGetSingleBrandQuery.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MusicCenter.Application.DTO;
+using MusicCenter.Application.Exceptions;
+using MusicCenter.Application.Queries.BrandQueries;
+using MusicCenter.Domain.Entities;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Queries.BrandQueries
+{
+    public class EfGetSingleBrandQuery : IGetSingleBrandQuery
+    {
+        private readonly MusicCenterDbContext _context;
+        private readonly IMapper _mapper;
+
+        public int Id => 7;
+        public string Name => "Ef Get Single Brand";
+
+        public EfGetSingleBrandQuery(MusicCenterDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this._mapper = mapper;
+        }
+
+        public BrandDto Execute(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if (brand == null)
+            {
+                throw new EntityNotFoundException(typeof(Brand), id);
+            }
+
+            return _mapper.Map<BrandDto>(brand);
+        }
+    }
+}
