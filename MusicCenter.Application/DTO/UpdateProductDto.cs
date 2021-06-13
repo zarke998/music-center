3475921242d778d2c5804f3c02ef9eff895f4d89
Andrej <zarke998@gmail.com>
2021-06-13 11:47:14 +0200

    feat: add UpdateProductValidator and dto

---
+++ MusicCenter.Application/DTO/UpdateProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.DTO
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public float? Discount { get; set; }
+        public string ImageUrl { get; set; }
+
+        public int? BrandId { get; set; }
+    }
+}
---
+++ MusicCenter.Implementation/Validators/UpdateProductValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using MusicCenter.Application;
+using MusicCenter.Application.DTO;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Validators
+{
+    public class UpdateProductValidator : AbstractValidator<UpdateProductDto>
+    {
+        public UpdateProductValidator(MusicCenterDbContext context)
+        {
+            RuleFor(dto => dto.Id)
+                .Must(id => context.Products.Any(p => p.Id == id))
+                .WithMessage("Product with given id does not exist")
+                .DependentRules(() =>
+                {
+                    RuleFor(dto => dto.Name)
+                        .Must((dto, name) => !context.Products.Any(p => p.Id != dto.Id && p.Name.ToLower() == name.ToLower()))
+                        .WithMessage("Product name must be unique.")
+                        .When(dto => !String.IsNullOrEmpty(dto.Name));
+
+                    RuleFor(dto => dto.Price)
+                        .GreaterThanOrEqualTo(0M)
+                        .WithMessage("Price must be positive number.")
+                        .When(dto => dto.Price.HasValue);
+
+                    RuleFor(dto => dto.Discount)
+                        .InclusiveBetween(0, 99)
+                        .WithMessage("Discount must be between 0 and 99 percent.")
+                        .When(dto => dto.Discount.HasValue);
+
+                    RuleFor(dto => dto.BrandId)
+                        .Must(brandId => context.Brands.Any(b => b.Id == brandId))
+                        .WithMessage("Brand with specified id does not exist.")
+                        .When(dto => dto.BrandId.HasValue);
+                });
+            
+        }
+    }
+}
