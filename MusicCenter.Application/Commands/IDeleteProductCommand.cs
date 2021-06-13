a52eca048cc61dcfa9a2fb0db8b52701e6d75eca
Andrej <zarke998@gmail.com>
2021-06-13 17:34:58 +0200

    feat: add EfDeleteProductCommand

---
+++ MusicCenter.Application/Commands/IDeleteProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Application.Commands
+{
+    public interface IDeleteProductCommand : ICommand<int>
+    {
+
+    }
+}
---
+++ MusicCenter.Implementation/Commands/EfDeleteProductCommand.cs
@@ -0,0 +1,31 @@
+﻿using MusicCenter.Application.Commands;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Commands
+{
+    public class EfDeleteProductCommand : IDeleteProductCommand
+    {
+        private readonly MusicCenterDbContext _context;
+
+        public int Id => 5;
+        public string Name => "Ef Delete Product";
+
+        public EfDeleteProductCommand(MusicCenterDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Execute(int request)
+        {
+            var product = _context.Products.Find(request);
+            _context.Products.Remove(product);
+
+            _context.SaveChanges();
+        }
+    }
+}
