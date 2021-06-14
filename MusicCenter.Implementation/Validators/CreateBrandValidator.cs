4e8c0762972b14306b8b9a77d572e14a30ff0fa5
Andrej <zarke998@gmail.com>
2021-06-14 12:58:45 +0200

    feat: add CreateBrandValidator

---
+++ MusicCenter.Implementation/Validators/CreateBrandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MusicCenter.Application.DTO;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Validators
+{
+    public class CreateBrandValidator : AbstractValidator<CreateBrandDto>
+    {
+        public CreateBrandValidator(MusicCenterDbContext context)
+        {
+            RuleFor(dto => dto.Name)
+                .NotEmpty()
+                .WithMessage("Brand name must not be empty")
+                .Must(name => !context.Brands.Any(b => b.Name.ToLower() == name.ToLower()))
+                .WithMessage("Brand name must be unique");
+        }
+    }
+}
