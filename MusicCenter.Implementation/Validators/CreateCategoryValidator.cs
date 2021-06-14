c6485ba09ebec7c93d556687c83abdc801cd803a
Andrej <zarke998@gmail.com>
2021-06-14 16:57:31 +0200

    feat: add CreateCategoryValidator

---
+++ MusicCenter.Implementation/Validators/CreateCategoryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MusicCenter.Application.DTO;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Validators
+{
+    public class CreateCategoryValidator : AbstractValidator<CategoryDto>
+    {
+        public CreateCategoryValidator(MusicCenterDbContext context)
+        {
+            RuleFor(dto => dto.Name)
+                .NotEmpty()
+                .WithMessage("Category name must not be empty")
+                .DependentRules(() =>
+                {
+                    RuleFor(dto => dto.Name)
+                        .Must(name => !context.Categories.Any(c => c.Name.ToLower() == name.ToLower()))
+                        .WithMessage("Category name must be unique");
+                });
+                
+        }
+    }
+}
