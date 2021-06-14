aa0e0e15a8c3d45f2e110805213a2959057f71fb
Andrej <zarke998@gmail.com>
2021-06-14 17:55:46 +0200

    feat(api): add user controller

---
+++ MusicCenter.API/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicCenter.Application;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UseCaseExecutor _executor;
+        private readonly MusicCenterDbContext _context;
+
+        public UsersController(UseCaseExecutor executor, IApplicationActor actor, MusicCenterDbContext context)
+        {
+            _executor = executor;            
+            _context = context;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
