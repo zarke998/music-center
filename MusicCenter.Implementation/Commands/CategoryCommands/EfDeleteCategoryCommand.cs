e6df8b971cee79412ab0abd4c6d428b080ada2ea
Andrej <zarke998@gmail.com>
2021-06-14 17:30:56 +0200

    feat: add EfDeleteCategoryCommand

---
+++ MusicCenter.Implementation/Commands/CategoryCommands/EfDeleteCategoryCommand.cs
@@ -0,0 +1,32 @@
+﻿using MusicCenter.Application.Commands.CategoryCommands;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Commands.CategoryCommands
+{
+    public class EfDeleteCategoryCommand : IDeleteCategoryCommand
+    {
+        private readonly MusicCenterDbContext _context;
+
+        public int Id => 13;
+        public string Name => "Ef Delete Category";
+
+        public EfDeleteCategoryCommand(MusicCenterDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Execute(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            _context.Categories.Remove(category);
+
+            _context.SaveChanges();
+        }
+    }
+}
