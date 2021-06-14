480cd50a3c347240f9c1677f8c197b5f8bfbd0ac
Andrej <zarke998@gmail.com>
2021-06-14 16:18:38 +0200

    feat(api): search categories

---
+++ MusicCenter.API/Controllers/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicCenter.Application;
+using MusicCenter.Application.Queries.CategoryQueries;
+using MusicCenter.Application.Searches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly UseCaseExecutor _executor;
+
+        public CategoriesController(UseCaseExecutor executor)
+        {
+            this._executor = executor;
+        }
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] CategorySearch search,
+                                    [FromServices] IGetCategoriesQuery query)
+        {
+            return Ok(_executor.ExecuteQuery(query, search));
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MusicCenter.API/Core/ContainerExtensions.cs
+++ MusicCenter.API/Core/ContainerExtensions.cs
@@ -6,11 +6,13 @@ using MusicCenter.Application;
 using MusicCenter.Application.Commands.BrandCommands;
 using MusicCenter.Application.Commands.ProductCommands;
 using MusicCenter.Application.Queries.BrandQueries;
+using MusicCenter.Application.Queries.CategoryQueries;
 using MusicCenter.Application.Queries.ProductQueries;
 using MusicCenter.EfDataAccess;
 using MusicCenter.Implementation.Commands.BrandCommands;
 using MusicCenter.Implementation.Commands.ProductCommands;
 using MusicCenter.Implementation.Queries.BrandQueries;
+using MusicCenter.Implementation.Queries.CategoryQueries;
 using MusicCenter.Implementation.Queries.ProductQueries;
 using MusicCenter.Implementation.Validators;
 using Newtonsoft.Json;
@@ -40,6 +42,10 @@ namespace MusicCenter.API.Core
             services.AddTransient<ICreateBrandCommand, EfCreateBrandCommand>();
             services.AddTransient<IUpdateBrandCommand, EfUpdateBrandCommand>();
             services.AddTransient<IDeleteBrandCommand, EfDeleteBrandCommand>();
+            #endregion
+
+            #region Category Use Cases
+            services.AddTransient<IGetCategoriesQuery, EfGetCategoriesQuery>();
             #endregion
         }
 
