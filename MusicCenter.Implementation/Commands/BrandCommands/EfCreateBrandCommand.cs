8a8ad2a4c4f976d1ecae5398b1433f19b908c655
Andrej <zarke998@gmail.com>
2021-06-14 12:00:28 +0200

    feat: add EfCreateBrandCommand

---
+++ MusicCenter.Implementation/Commands/BrandCommands/EfCreateBrandCommand.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MusicCenter.Application.Commands.BrandCommands;
+using MusicCenter.Application.DTO;
+using MusicCenter.Domain.Entities;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Commands.BrandCommands
+{
+    public class EfCreateBrandCommand : ICreateBrandCommand
+    {
+        private readonly MusicCenterDbContext _context;
+        private readonly IMapper _mapper;
+
+        public int Id => 8;
+        public string Name => "Ef Create Brand";
+
+        public EfCreateBrandCommand(MusicCenterDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Execute(CreateBrandDto dto)
+        {
+            var newBrand = _mapper.Map<Brand>(dto);
+
+            _context.Brands.Add(newBrand);
+            _context.SaveChanges();
+        }
+    }
+}
