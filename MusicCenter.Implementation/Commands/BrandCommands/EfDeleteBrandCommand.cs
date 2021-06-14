13a1fbe852d4b40bea6750a1221d6bff25556368
Andrej <zarke998@gmail.com>
2021-06-14 13:42:44 +0200

    feat: add EfDeleteBrandCommand

---
+++ MusicCenter.Implementation/Commands/BrandCommands/EfDeleteBrandCommand.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MusicCenter.Application.Commands.BrandCommands;
+using MusicCenter.EfDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation.Commands.BrandCommands
+{
+    public class EfDeleteBrandCommand : IDeleteBrandCommand
+    {
+        private readonly MusicCenterDbContext _context;
+        private readonly IMapper _mapper;
+
+        public EfDeleteBrandCommand(MusicCenterDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public int Id => 10;
+        public string Name => "Ef Delete Brand";
+
+        public void Execute(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            _context.Brands.Remove(brand);
+
+            _context.SaveChanges();
+        }
+    }
+}
