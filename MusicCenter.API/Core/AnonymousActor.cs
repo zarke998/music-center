4aa71d8155103dfc98b20709b384ffce433cb4af
Andrej <zarke998@gmail.com>
2021-06-16 21:45:19 +0200

    feat(api): add default use cases for anonym user

--- MusicCenter.API/Core/AnonymousActor.cs
+++ MusicCenter.API/Core/AnonymousActor.cs
@@ -1,4 +1,5 @@
 ﻿using MusicCenter.Application;
+using MusicCenter.Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,18 @@ namespace MusicCenter.API.Core
         public int Id { get; set; }
         public string Identity { get; set; } = "Anonymous";
         public ICollection<int> AllowedUseCases { get; set; } = new List<int>();
+
+        public AnonymousActor()
+        {
+            var getUseCases = UseCaseUtils.AllUseCases.Where(uc => uc.Name.Contains("Get"))
+                                                        .Select(uc => uc.Id);
+
+            var registerUseCase = UseCaseUtils.AllUseCases.First(uc => uc.Name == "Ef Create User").Id;
+
+            foreach (var getUseCase in getUseCases)
+                AllowedUseCases.Add(getUseCase);
+
+            AllowedUseCases.Add(registerUseCase);
+        }
     }
 }
---
+++ MusicCenter.Implementation/UseCaseUtils.cs
@@ -0,0 +1,53 @@
+﻿using MusicCenter.Application;
+using MusicCenter.Implementation.Commands.ProductCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCenter.Implementation
+{
+    public static class UseCaseUtils
+    {
+        private static List<UseCaseInfo> _allUseCases;
+        public static List<UseCaseInfo> AllUseCases
+        {
+            get
+            {
+                if(_allUseCases == null)
+                {
+                    _allUseCases = GetAllUseCases();
+                }
+                return _allUseCases;
+            }
+        }
+
+        private static List<UseCaseInfo> GetAllUseCases()
+        {
+            var allUseCaseTypes = typeof(EfDeleteProductCommand).Assembly.GetTypes()
+                                .Where(t => t.IsAssignableTo(typeof(IUseCase)));
+
+            var allUseCases = new List<UseCaseInfo>();
+
+            foreach (var useCase in allUseCaseTypes)
+            {
+                var useCaseInstance = FormatterServices.GetUninitializedObject(useCase) as IUseCase;
+                allUseCases.Add(new UseCaseInfo()
+                {
+                    Id = useCaseInstance.Id,
+                    Name = useCaseInstance.Name
+                });
+            }
+
+            return allUseCases;
+        }
+    }
+
+    public class UseCaseInfo
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
